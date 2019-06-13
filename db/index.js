2759f42a51f9fab66992980568fef3f68477ba96
Dan Orel <mail.ordan@gmail.com>
2019-06-13 21:26:44 +0300

    Improvising with node.js

--- db/index.js
+++ db/index.js
@@ -1,13 +1,22 @@
+let util = require('util');
 let phrases;
 
 exports.connect = function(){
-    phrases = require('./ru');
+    phrases = require('./eng');
 };
 
 exports.getPhrase = function(name) {
     if(!phrases[name]){
-        throw new Error("Such phrase is undefined in the db!");
+        throw new PhraseError(500, "Such phrase is undefined in the db!");
     } else {
         return phrases[name];
     }
 };
+
+function PhraseError(status, msg){
+    this.msg = msg;
+    this.status = status;
+    Error.captureStackTrace(this, PhraseError);
+}
+util.inherits(PhraseError, Error);
+PhraseError.prototype.name = "PhraseError";
--- error.js
+++ error.js
@@ -1,15 +1,6 @@
 let util = require('util');
-let eng = require('./db/eng');
 let db = require('db');
 db.connect();
-
-function PhraseError(status, msg){
-    this.msg = msg;
-    this.status = status;
-    Error.captureStackTrace(this, PhraseError);
-}
-util.inherits(PhraseError, Error);
-PhraseError.prototype.name = "PhraseError";
 
 function HttpError(status, msg){
     this.msg = msg;
@@ -23,16 +14,15 @@ function makePage(url){
     if(url !== "index.html"){
         throw new HttpError(404, "Error. Could not found the page!");
     } else {
-       return util.format('%s, %s!', db.getPhrase("Hello"), db.getPhrase("world"));
+       return util.format('%s, %s!', db.getPhrase("Hell"), db.getPhrase("world"));
     }
 }
 
 try{
-    let page = makePage("index");
-    console.log(page);
+    let page = makePage('index.html');
 } catch(exception){
     if(exception instanceof HttpError){
-        console.log(exception.status, exception.msg);
+        console.warn(exception.status, exception.msg);
     } else {
         console.error(exception.name, exception.msg, exception.stack);
     }
